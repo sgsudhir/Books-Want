6fcb512b122ea708ea15ee2d4c2080167d4d1f43
sgsudhir <sgsudhir@hotmail.com>
2017-04-28 18:13:33 +0530

    Include and install Volley network library to request get JSON and Bitmap image in an easy way

--- app/src/main/java/com/imca2017/bookswant/MainActivity.java
+++ app/src/main/java/com/imca2017/bookswant/MainActivity.java
@@ -1,22 +1,36 @@
 package com.imca2017.bookswant;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.speech.RecognizerIntent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.imca2017.bookswant.app.AppController;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private MaterialSearchView searchView;
     ArrayList<String> sugg = new ArrayList<String>();
+    ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
+                //Request through Volly Network
+                loadData();
                 return false;
             }
 
@@ -65,6 +81,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    //An Example of fatching JSON using Volly network Library
+    private void loadData() {
+        String url= "https://www.googleapis.com/books/v1/volumes?q=you+can+win&&maxResults=10";
+
+        pDialog = new ProgressDialog(MainActivity.this);
+
+        String tag_json_obj = "json_obj_req";
+
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("--Results--", response.toString());
+                        pDialog.hide();
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("--Results--", error.getMessage());
+                pDialog.hide();
+
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(request, tag_json_obj);
     }
 
     @Override
