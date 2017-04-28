4040a21c06e7706ff9c05a3d59a0aba8ccac3ca5
sgsudhir <sgsudhir@hotmail.com>
2017-04-29 04:39:36 +0530

    Add helper.UrlGenerator and UI to generate request URL

--- app/src/main/java/com/imca2017/bookswant/MainActivity.java
+++ app/src/main/java/com/imca2017/bookswant/MainActivity.java
@@ -6,43 +6,46 @@ import android.graphics.Color;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.imca2017.bookswant.app.AppController;
+import com.imca2017.bookswant.helper.UrlGenerator;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private MaterialSearchView searchView;
     ArrayList<String> sugg = new ArrayList<String>();
     ProgressDialog pDialog;
+    SwitchCompat switchCompat;
+    UrlGenerator urlGenerator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setBackgroundColor(Color.parseColor("#FF5E5E5E"));
+        urlGenerator = new UrlGenerator();
 
         searchView = (MaterialSearchView) findViewById(R.id.search_view);
-        searchView.setVoiceSearch(true);
+        searchView.setVoiceSearch(false);
         searchView.setCursorDrawable(R.drawable.custom_cursor);
         searchView.setEllipsize(true);
         //Dummy Data for the Demo
@@ -59,17 +62,16 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                //Request through Volly Network
-                loadData();
+                Log.d("-----SEARCH URL-----", urlGenerator.getURL());
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                urlGenerator.setSearchQuery(newText);
                 return false;
             }
         });
-
         searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
             @Override
             public void onSearchViewShown() {
@@ -82,6 +84,74 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        switchCompat = (SwitchCompat) findViewById(R.id.content_main_switch_download);
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    urlGenerator.setDownload(true);
+                } else if (!isChecked) {
+                    urlGenerator.setDownload(false);
+                }
+            }
+        });
+
+    }
+
+    public void orderbyRadioButtonClicked(View view) {
+        switch (view.getId()) {
+            case R.id.content_main_radio_orderby_newest:
+                urlGenerator.setOrderBy(UrlGenerator.ORDERBY_NEWEST);
+                break;
+            case R.id.content_main_radio_orderby_relevance:
+                urlGenerator.setOrderBy(UrlGenerator.ORDERBY_RELEVANCE);
+                break;
+            case R.id.content_main_radio_orderby_anything:
+                urlGenerator.setOrderBy(UrlGenerator.ORDERBY_ANYTHING);
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    public void printtypeRadioButtonClicked(View view) {
+        switch (view.getId()) {
+            case R.id.content_main_radio_printtype_all:
+                urlGenerator.setPrintType(UrlGenerator.PRINT_TYPE_ALL);
+                break;
+            case R.id.content_main_radio_printtype_books:
+                urlGenerator.setPrintType(UrlGenerator.PRINT_TYPE_BOOKS);
+                break;
+            case R.id.content_main_radio_printtype_magazines:
+                urlGenerator.setPrintType(UrlGenerator.PRINT_TYPE_MAGAZINES);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void filterbyRadioButtonClicked(View view) {
+        switch (view.getId()) {
+            case R.id.content_main_radio_filterby_all:
+                urlGenerator.setFilter(UrlGenerator.FILTER_BY_NONE);
+                break;
+            case R.id.content_main_radio_filterby_free:
+                urlGenerator.setFilter(UrlGenerator.FILTER_BY_FREE_EBOOKS);
+                break;
+            case R.id.content_main_radio_filterby_paid:
+                urlGenerator.setFilter(UrlGenerator.FILTER_BY_PAID_EBOOKS);
+                break;
+            case R.id.content_main_radio_filterby_full:
+                urlGenerator.setFilter(UrlGenerator.FILTER_BY_FULL_EBOOKS);
+                break;
+            case R.id.content_main_radio_filterby_sample:
+                urlGenerator.setFilter(UrlGenerator.FILTER_BY_SAMPLE_EBOOKS);
+                break;
+            default:
+                break;
+        }
     }
 
     //An Example of fatching JSON using Volly network Library
---
+++ app/src/main/java/com/imca2017/bookswant/helper/UrlGenerator.java
@@ -0,0 +1,73 @@
+package com.imca2017.bookswant.helper;
+
+/**
+ * Created by sgsudhir on 29/4/17.
+ */
+
+public class UrlGenerator {
+    public static String EPUB = "epub";
+    public static String ORDERBY_NEWEST = "newest";
+    public static String ORDERBY_RELEVANCE = "relevance";
+    public static String ORDERBY_ANYTHING = null;
+    public static String PRINT_TYPE_ALL = "all";
+    public static String PRINT_TYPE_BOOKS = "books";
+    public static String PRINT_TYPE_MAGAZINES = "magazines";
+    public static String FILTER_BY_NONE = "ebooks";
+    public static String FILTER_BY_FREE_EBOOKS = "free-ebooks";
+    public static String FILTER_BY_PAID_EBOOKS = "paid-ebooks";
+    public static String FILTER_BY_FULL_EBOOKS = "full";
+    public static String FILTER_BY_SAMPLE_EBOOKS = "partial";
+
+    /**
+     * Example search query -
+     * https://www.googleapis.com/books/v1/volumes?q=you+can+win&&orderBy=newest&&filter=full
+     */
+    public static String BASE_URL = "https://www.googleapis.com/books/v1/volumes";
+
+    private String searchQuery;
+    private boolean download;
+    private String filter;
+    private String orderBy;
+    private String printType;
+    private String url;
+
+    public UrlGenerator() {
+        this.download = false;
+        this.filter = FILTER_BY_NONE;
+        this.orderBy = ORDERBY_ANYTHING;
+        this.printType = PRINT_TYPE_ALL;
+        this.url = null;
+    }
+
+    public String getURL() {
+        this.url = this.BASE_URL + "?q=" + this.searchQuery + "&&filter=" + this.filter + "&&printType=" + this.printType;
+        if (download) {
+            this.url = this.url + "&&download=epub";
+        }
+        if (this.orderBy != null && !this.orderBy.isEmpty()) {
+            this.url = this.url + "&&orderBy=" + this.orderBy;
+        }
+        return url;
+    }
+
+    public void setSearchQuery(String searchQuery) {
+        this.searchQuery = searchQuery;
+    }
+
+    public void setDownload(boolean download) {
+        this.download = download;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public void setPrintType(String printType) {
+        this.printType = printType;
+    }
+
+}
